5fd0b341d6b58e7eecae80772c815098ba9cfac4
Hoang Do <katievincy226@gmail.com>
2020-07-16 12:51:30 -0400

    Initial Commit

---
+++ Header.php
@@ -0,0 +1,3 @@
+<?php
+echo "<div id=\"header\"><h1>Welcome to my websites</h1></div>";
+?>
---
+++ Menu.php
@@ -0,0 +1,9 @@
+<?php
+echo "<div id=\"menu\">";
+echo    "<ul>";
+echo        "<li><a href=\"ChessBoard.php\">Chess Board</a></li>";
+echo        "<li><a href=\"Prime.php\">Prime</a></li>";
+echo        "<li><a href=\"Pattern.php\">Pattern</a></li>";
+echo    "</ul>";
+echo "</div>";
+?>
